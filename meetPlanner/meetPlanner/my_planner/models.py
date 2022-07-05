e3e66df9ad692440083ff66791c9bc355c7ba727
Valdas333 <valdemarasged@gmail.com>
2022-07-05 17:01:09 +0300

    pervadinti stulpeliu pavadinimai i angliskus

--- meetPlanner/meetPlanner/my_planner/models.py
+++ meetPlanner/meetPlanner/my_planner/models.py
@@ -1,32 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
 class Person(models.Model):
-    name = models.CharField('Vardas', max_length=100, blank=False)
-    surname = models.CharField('Pavarde', max_length=100, blank=False)
+    name = models.CharField('Name', max_length=100, blank=False)
+    surname = models.CharField('Surname', max_length=100, blank=False)
     DUTIES_FIXED_VALUES = [
         ('a', 'Developer'),
         ('b', 'Support'),
         ('c', 'Testing'),
         ('d', 'Management'),
     ]
-    duties = models.CharField('Pareigos', choices= DUTIES_FIXED_VALUES, blank=False, max_length=50)
-    email = models.EmailField('Elektroninis pastas', max_length=254, blank=False)
+    duties = models.CharField('Duties', choices= DUTIES_FIXED_VALUES, blank=False, max_length=50)
+    email = models.EmailField('Email', max_length=254, blank=False)
     
     def __str__(self):
         return f"{self.name} {self.surname} {self.get_duties_display()}"
     
+    
 class Meeting(models.Model):
     title = models.CharField('Title', max_length=10)
-    responsible_person = models.ForeignKey(to='Person', verbose_name='Atsakingas asmuo', on_delete=models.SET_NULL, null=True)
-    description = models.CharField('Aprasymas', max_length=50, help_text='trumpas aprasymas, apie 50 simboliu')
+    responsible_person = models.ForeignKey(to='Person', verbose_name='Responsible person', on_delete=models.SET_NULL, null=True)
+    description = models.CharField('Aprasymas', max_length=50, help_text='Description about 50 simbols')
     CATEGORY_FIXED_VALUES = [
         ('a', 'CodeMonkey'),
         ('b', 'Hub'),
         ('c', 'Short'),
         ('d', 'TeamBuilding'),
     ]
-    category = models.CharField(max_length=1, choices= CATEGORY_FIXED_VALUES, blank=True, default='a', help_text='kategorija')
+    category = models.CharField(max_length=1, choices= CATEGORY_FIXED_VALUES, blank=True, default='a', help_text='Category')
     TYPE_FIXED_VALUES = [
         ('a', 'Live'),
         ('b', 'InPerson'),
@@ -37,4 +38,4 @@ class Meeting(models.Model):
     create_time = models.DateTimeField(auto_now_add=True, editable=False)
     
     def __str__(self):
-        return f"Meetas {self.title}, paskirtas laikas nuo {self.start_time} iki {self.end_time}, sukurtas {self.create_time}"
+        return f"Meet {self.title} is scheduled from {self.start_time} until {self.end_time}, created @{self.create_time}"
